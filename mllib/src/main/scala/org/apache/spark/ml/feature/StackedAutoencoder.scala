7126e008a370dd8931d2e2542028ebdad946a821
Alexander Ulanov <nashb@yandex.ru>
2015-11-12 19:34:23 -0800

    Initial stacked autoencoder commit

--- mllib/src/main/scala/org/apache/spark/ml/feature/StackedAutoencoder.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/StackedAutoencoder.scala
@@ -18,48 +18,24 @@
 package org.apache.spark.ml.feature
 
 import org.apache.spark.annotation.Experimental
-import org.apache.spark.ml.ann._
-import org.apache.spark.ml.classification.MultilayerPerceptronParams
-import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
+import org.apache.spark.ml.ann.{FeedForwardTrainer, EmptyLayerWithSquaredError, FeedForwardTopology}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.ml.{Model, Estimator}
-import org.apache.spark.ml.param._
+import org.apache.spark.ml.classification.MultilayerPerceptronParams
+import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.mllib.linalg.{VectorUDT, Vectors, Vector}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Row, DataFrame}
 import org.apache.spark.sql.types.{StructField, StructType}
 
 import breeze.linalg.{DenseVector => BDV}
 
-/**
- * Params for [[Autoencoder]] and [[AutoencoderModel]].
- */
-private[feature] trait AutoencoderParams extends Params with HasInputCol with HasOutputCol {
-  /**
-   * True if data is in [0, 1] interval.
-   * Default: false
-   * @group expertParam
-   */
-  final val dataIn01Interval: BooleanParam = new BooleanParam(this, "dataIn01Interval",
-    "True if data is in [0, 1] interval." +
-      " Sets the layer on the top of the autoencoder: linear + sigmoid (true) " +
-      " or linear (false)")
 
-  /** @group getParam */
-  final def getDataIn01Interval: Boolean = $(dataIn01Interval)
-
-}
-
-/**
- * :: Experimental ::
- * Autoencoder.
- */
 @Experimental
-class Autoencoder (override val uid: String) extends Estimator[AutoencoderModel]
-  with MultilayerPerceptronParams with AutoencoderParams  {
+class StackedAutoencoder (override val uid: String)
+  extends Estimator[StackedAutoencoderModel]
+  with MultilayerPerceptronParams with AutoencoderParams {
 
-  def this() = this(Identifiable.randomUID("autoencoder"))
+  def this() = this(Identifiable.randomUID("stackedAutoencoder"))
 
   /** @group setParam */
   def setDataIn01Interval(value: Boolean): this.type = set(dataIn01Interval, value)
@@ -98,23 +74,52 @@ class Autoencoder (override val uid: String) extends Estimator[AutoencoderModel]
    */
   def setSeed(value: Long): this.type = set(seed, value)
 
+
+
   /**
    * Fits a model to the input data.
-   * @param dataset dataset
-   * @return encoder and decoder models
    */
-  override def fit(dataset: DataFrame): AutoencoderModel = {
+  override def fit(dataset: DataFrame): StackedAutoencoderModel = {
     val data = dataset.select($(inputCol)).map { case Row(x: Vector) => (x, x) }
     val linearOutput = !$(dataIn01Interval)
-    val topology = FeedForwardTopology.multiLayerPerceptron($(layers), false)
-    if (linearOutput) topology.layers(topology.layers.length - 1) = new EmptyLayerWithSquaredError()
-    val FeedForwardTrainer = new FeedForwardTrainer(topology, $(layers)(0), $(layers).last)
-    FeedForwardTrainer.LBFGSOptimizer
+    // TODO: make a loop for creating of Autoencoders
+    for (i <- 0 until $(layers).length - 2) {
+      // Train autoencoder for each layer except the last
+      val currentLayers = Array($(layers)(i), $(layers)(i + 1), $(layers)(i))
+      val topology = FeedForwardTopology.multiLayerPerceptron(currentLayers, false)
+      val isLastWithLinear = i == $(layers).length - 3 && linearOutput
+      if (isLastWithLinear) {
+        topology.layers(topology.layers.length - 1) = new EmptyLayerWithSquaredError()
+      }
+      val FeedForwardTrainer = new FeedForwardTrainer(topology, $(layers)(0), $(layers).last)
+      FeedForwardTrainer.LBFGSOptimizer
         .setConvergenceTol($(tol))
         .setNumIterations($(maxIter))
-    FeedForwardTrainer.setStackSize($(blockSize))
-    val encoderDecoderModel = FeedForwardTrainer.train(data)
-    new AutoencoderModel(uid + "decoder", $(layers), encoderDecoderModel.weights(), linearOutput)
+      FeedForwardTrainer.setStackSize($(blockSize))
+      val encoderDecoderModel = FeedForwardTrainer.train(data)
+      val encoderDecoderWeights = encoderDecoderModel.weights().toArray
+      // derive encoder and decoder
+      var coderWeightSize = 0
+      for (layer <- topology.layers) {
+        coderWeightSize += layer.weightSize
+      }
+      val decoderTopology = FeedForwardTopology.multiLayerPerceptron(currentLayers.tail, false)
+      if (isLastWithLinear) {
+        decoderTopology.layers(topology.layers.length - 1) = new EmptyLayerWithSquaredError()
+      }
+      val decoderWeights =
+        Vectors.fromBreeze(
+          new BDV(encoderDecoderWeights.toArray, encoderDecoderWeights.size - coderWeightSize))
+
+      val decoderModel = topology.getInstance(decoderWeights)
+      val encoderTopology = FeedForwardTopology.multiLayerPerceptron(currentLayers.init, false)
+      val encoderWeights =
+      Vectors.fromBreeze(new BDV(encoderDecoderWeights, 0, 1, coderWeightSize))
+      val encoderModel = encoderTopology.getInstance(encoderWeights)
+      // TODO: unite en and de into the stack.
+    }
+    // TODO: pass relevant parameters. How to pass weights?
+    new StackedAutoencoderModel(uid + "decoder", $(layers), null, linearOutput)
   }
 
   override def copy(extra: ParamMap): Estimator[AutoencoderModel] = defaultCopy(extra)
@@ -134,64 +139,24 @@ class Autoencoder (override val uid: String) extends Estimator[AutoencoderModel]
     StructType(outputFields)
   }
 }
-/**
- * :: Experimental ::
- * Autoencoder model.
- *
- * @param layers array of layer sizes including input and output
- * @param weights weights (or parameters) of the model
- */
+
 @Experimental
-class AutoencoderModel private[ml] (
+class StackedAutoencoderModel private[ml] (
     override val uid: String,
     layers: Array[Int],
     weights: Vector,
-    linearOutput: Boolean) extends Model[AutoencoderModel] with AutoencoderParams {
+    linearOutput: Boolean) extends Model[StackedAutoencoderModel] with AutoencoderParams {
 
-  /** @group setParam */
-  def setInputCol(value: String): this.type = set(inputCol, value)
-
-  /** @group setParam */
-  def setOutputCol(value: String): this.type = set(outputCol, value)
-
-  private var coderWeightSize = 0
-  private val decoderModel = {
-    val topology = FeedForwardTopology.multiLayerPerceptron(layers.tail, false)
-    for (layer <- topology.layers) {
-      coderWeightSize += layer.weightSize
-    }
-    if (linearOutput) topology.layers(topology.layers.length - 1) = new EmptyLayerWithSquaredError()
-    val decoderWeights =
-      Vectors.fromBreeze(new BDV(weights.toArray, weights.size - coderWeightSize))
-    topology.getInstance(decoderWeights)
-  }
-  private val encoderModel = {
-    val topology = FeedForwardTopology.multiLayerPerceptron(layers.init, false)
-    val encoderWeights =
-      Vectors.fromBreeze(new BDV(weights.toArray, 0, 1, coderWeightSize))
-    topology.getInstance(encoderWeights)
-  }
-
-
-  override def copy(extra: ParamMap): AutoencoderModel = {
-    copyValues(new AutoencoderModel(uid, layers, weights, linearOutput), extra)
+  override def copy(extra: ParamMap): StackedAutoencoderModel = {
+    copyValues(new StackedAutoencoderModel(uid, layers, weights, linearOutput), extra)
   }
 
   /**
    * Transforms the input dataset.
    */
   override def transform(dataset: DataFrame): DataFrame = {
-    transformSchema(dataset.schema, logging = true)
-    val pcaOp = udf { encoderModel.predict _ }
-    dataset.withColumn($(outputCol), pcaOp(col($(inputCol))))
-  }
-
-  def encode(dataset: DataFrame): DataFrame = transform(dataset)
-
-  def decode(dataset: DataFrame): DataFrame = {
-    transformSchema(dataset.schema, logging = true)
-    val pcaOp = udf { decoderModel.predict _ }
-    dataset.withColumn($(outputCol), pcaOp(col($(inputCol))))
+    // TODO: implement
+    null
   }
 
   /**
